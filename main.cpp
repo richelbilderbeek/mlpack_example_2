4c3be8cca625471338b6b1627c39886d46babba0
richelbilderbeek <richel@richelbilderbeek.nl>
2022-09-15 09:26:23 +0200

    Expose https://github.com/mlpack/mlpack/issues/3274

--- main.cpp
+++ main.cpp
@@ -1,37 +1,51 @@
 #include <mlpack/core.hpp>
-#include <mlpack/methods/neighbor_search/neighbor_search.hpp>
+#include <mlpack/methods/reinforcement_learning/q_learning.hpp>
 
+/*
+#include <mlpack/core.hpp>
+#include <mlpack/methods/ann/ffn.hpp>
+#include <mlpack/methods/ann/init_rules/gaussian_init.hpp>
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
+#include <mlpack/methods/reinforcement_learning/q_learning.hpp>
+#include <mlpack/methods/reinforcement_learning/q_networks/simple_dqn.hpp>
+#include <mlpack/methods/reinforcement_learning/environment/cart_pole.hpp>
+#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
+#include <mlpack/methods/reinforcement_learning/training_config.hpp>
+#include <ensmallen.hpp>
 using namespace mlpack;
-using namespace mlpack::neighbor; // NeighborSearch and NearestNeighborSort
-using namespace mlpack::metric; // ManhattanDistance
+using namespace mlpack::ann;
+using namespace ens;
+using namespace mlpack::rl;
+*/
+
+/*
+//#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
+
+struct action
+{
+
+};
+
+struct environment
+{
+  using Action = action;
+};
+
+struct state
+{
+  bool is_alive;
+  bool has_vital_item;
+};
 
 int main()
 {
-  // Armadillo is a C++ linear algebra library; mlpack uses its matrix data type.
-  arma::mat data;
+  //mlpack::rl::GreedyPolicy<environment> policy(1.0, 1000, 0.1);
 
- /*
-   * Create a NeighborSearch model. The parameters of the model are specified
-   * with templates:
-   *  - Sorting method: "NearestNeighborSort" - This class sorts by increasing
-   *    distance.
-   *  - Distance metric: "ManhattanDistance" - The L1 distance, sum of absolute
-   *    distances.
-   *
-   * Pass the reference dataset (the vectors to be searched through) to the
-   * constructor.
-   */
-  NeighborSearch<NearestNeighborSort, ManhattanDistance> nn;
 
-  /*
-   * Create the matrices to hold the results of the search.
-   *   neighbors [k  x  n] - Indeces of the nearest neighbor(s).
-   *                         One column per data query vector and one row per
-   *                        'k' neighbors.
-   *   distances [k  x  n] - Calculated distance values.
-   *                         One column per data query vector and one row per
-   *                        'k' neighbors.
-   */
-  arma::Mat<size_t> neighbors;
-  arma::mat distances;
+}
+*/
+
+int main()
+{
 }
