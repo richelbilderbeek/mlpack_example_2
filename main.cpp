573b6356bcd18f172d1bac905ae8be3daea4e218
richelbilderbeek <richel@richelbilderbeek.nl>
2022-09-18 11:35:57 +0200

    Do not build mlpack

--- main.cpp
+++ main.cpp
@@ -1,30 +1,8 @@
 #pragma GCC diagnostic ignored "-Wshadow"
 #include <mlpack/core.hpp>
 #include <mlpack/methods/reinforcement_learning/q_learning.hpp>
+#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
 #pragma GCC diagnostic pop
-
-int main()
-{
-}
-
-/*
-#include <mlpack/core.hpp>
-#include <mlpack/methods/ann/ffn.hpp>
-#include <mlpack/methods/ann/init_rules/gaussian_init.hpp>
-#include <mlpack/methods/ann/layer/layer.hpp>
-#include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
-#include <mlpack/methods/reinforcement_learning/q_learning.hpp>
-#include <mlpack/methods/reinforcement_learning/q_networks/simple_dqn.hpp>
-#include <mlpack/methods/reinforcement_learning/environment/cart_pole.hpp>
-#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
-#include <mlpack/methods/reinforcement_learning/training_config.hpp>
-#include <ensmallen.hpp>
-using namespace mlpack;
-using namespace mlpack::ann;
-using namespace ens;
-using namespace mlpack::rl;
-
-#include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
 
 struct action
 {
@@ -47,4 +25,3 @@ int main()
   mlpack::rl::GreedyPolicy<environment> policy(1.0, 1000, 0.1);
 
 }
-*/
