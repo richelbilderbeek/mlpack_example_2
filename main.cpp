e00b4b3d1253eb292dfe4383d38572b447f200c8
richelbilderbeek <richel@richelbilderbeek.nl>
2022-09-18 12:03:48 +0200

    Better give up?

--- main.cpp
+++ main.cpp
@@ -2,26 +2,77 @@
 #include <mlpack/core.hpp>
 #include <mlpack/methods/reinforcement_learning/q_learning.hpp>
 #include <mlpack/methods/reinforcement_learning/policy/greedy_policy.hpp>
+#include <mlpack/methods/reinforcement_learning/policy/policy.hpp>
+#include <mlpack/methods/reinforcement_learning/training_config.hpp>
+#include <mlpack/methods/reinforcement_learning/environment/env_type.hpp>
 #pragma GCC diagnostic pop
 
-struct action
+// Follow mlpack naming convention: class names start with uppercase
+struct Action
 {
 
 };
 
-struct environment
-{
-  using Action = action;
-};
-
-struct state
+// Follow mlpack naming convention: class names start with uppercase
+struct State
 {
   bool is_alive;
   bool has_vital_item;
 };
 
+// Follow mlpack naming convention: class names start with uppercase
+struct Environment
+{
+  using Action = ::Action;
+  using State = ::State;
+};
+
+
+// Follow mlpack naming convention: class names start with uppercase
+struct Network
+{
+
+};
+
+// Follow mlpack naming convention: class names start with uppercase
+using Policy = mlpack::rl::GreedyPolicy<Environment>;
+
+
+// Follow mlpack naming convention: class names start with uppercase
+struct Updater
+{
+  using Policy = ::Policy;
+};
+
+
+// Follow mlpack naming convention: class names start with uppercase
+struct Replay
+{
+  // No idea
+};
+
+
+// Follow mlpack naming convention: class names start with uppercase
+using Learner = mlpack::rl::QLearning<
+  Environment,
+  Network,
+  Updater,
+  Policy,
+  Replay
+>;
+
 int main()
 {
-  mlpack::rl::GreedyPolicy<environment> policy(1.0, 1000, 0.1);
-
+  /*
+  mlpack::rl::TrainingConfig config;
+  Network network;
+  const Policy policy(1.0, 1000, 0.1);
+  mlpack::rl::PrioritizedReplay<mlpack::rl::DiscreteActionEnv<1,2,3>> replayMethod(64, 5000, 0.6, 3);
+  const Learner learner(
+    config,
+    network,
+    policy,
+    replayMethod
+  );
+  */
 }
